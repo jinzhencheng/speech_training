46ec10c1788c05f4d32875afdd88e46d05958ce4
jinzc <jinzhencheng@auto-smart.com>
2018-01-16 10:11:36 +0800

    项目架构配置

---
+++ starter.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+# Created by Jin(jinzhencheng@outlook.com) at 2018/01/16.
+
+from application import app
+from application import web
+
+if __name__ == '__main__':
+    app.run()
